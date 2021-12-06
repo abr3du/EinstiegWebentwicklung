1dde187b965aa10f71fb0e8750fe55dd99b5569f
John Farmer <john.farmer@gmx.net>
2021-12-06 19:12:15 +0100

    versuch nicht zu löschen

--- JS/js_index.js
+++ JS/js_index.js
@@ -1,8 +1,14 @@
-// function myFunction() {
-//   var x = document.getElementById("myLinks");
-//   if (x.style.display === "block") {
-//     x.style.display = "none";
-//   } else {
-//     x.style.display = "block";
-//   }
+function myFunction() {
+  var x = document.getElementById("not_active");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+// var y = document.getElementById("not_active").querySelectorAll("a");
+// var i;
+// for (i = 0; i < y.length; i++) {
+//   y[i].style.backgroundColor = "green";
 // }
